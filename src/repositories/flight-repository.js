f3012de2c33c8221aac95e023b40145e7d1bb86f
harpreet0816 <harpreet8730@gmail.com>
2025-02-08 16:14:43 +0530

    added seats model in db associated to airplane

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -1,19 +1,54 @@
+const { Sequelize } = require("sequelize")
 const CrudRepository = require("./crud-repository");
-const { Flight } = require("../models")
+const { Flight, Airplane, Airport, City } = require("../models");
 
-class FlightRepository extends CrudRepository{
-    constructor(parameters) {
-        super(Flight);
-    }
+class FlightRepository extends CrudRepository {
+  constructor(parameters) {
+    super(Flight);
+  }
 
-    async getAllFlights (filter, sort){
-        const response = Flight.findAll({
-            where: filter,
-            order: sort
-        });
+  async getAllFlights(filter, sort) {
+    const response = Flight.findAll({
+      where: filter,
+      order: sort,
+      include: [
+        {
+          model: Airplane,
+          required: true, //for eagar loading inner join
+          as: "airplaneDetails" //same as the alias we set in the model assocations other wise give error
+        },
+        {
+            // by default it add a join query on airport.id but we want for airport.code for this we need to mention explicitly using on property 
 
-        return response;
-    }
+            // as should be same as the alias we add in model assocation and migrations 
+          model: Airport,
+          required: true, //for eagar loading inner join
+          as: "departureAirport",
+          on: {
+            col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
+          } ,
+          include: {
+            model: City,
+            required: true,
+          }
+        },
+        {
+          model: Airport,
+          required: true, //for eagar loading inner join
+          as: "arrivalAirport",
+          on: {
+            col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
+          } ,
+          include: {
+            model: City,
+            required: true,
+          }
+        },
+      ],
+    });
+
+    return response;
+  }
 }
 
 module.exports = FlightRepository;
